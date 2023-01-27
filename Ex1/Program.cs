0dd56362f81c3ab54707696fe0964d39d201d552
MatvienokVS <Skavaava@gmail.com>
2023-01-27 21:09:04 +0300

    Решение ДЗ семинар 7

---
+++ Ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу реализующую метод, который возвращает массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 - 2 - 0,2
+//1 - 3,3 8 - 9,9
+//8 7,8 - 7,1 9
+
+using static System.Console;
+Clear();
+
+
+
+
+int[,] GetDDArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] Array = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Array[i, j] = rnd.Next(minValue, maxValue+1);
+        }
+    }
+    return Array;
+}
