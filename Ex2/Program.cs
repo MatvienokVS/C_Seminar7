921de91d60c1dd376fcf417824e8452e1209f08e
MatvienokVS <skavaava@gmail.com>
2023-01-29 23:04:07 +0300

    Решение задач Семинар 7

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -4,3 +4,62 @@
 //5 9 2 3
 //8 4 2 4
 
+using static System.Console;
+Clear();
+
+Write("Введите параметры массива и искомое значение через пробел: ");
+
+int[] ints = Array.ConvertAll(ReadLine()!.Split(new char[] { ' ', '.', ',', ';', ':' }, StringSplitOptions.RemoveEmptyEntries), int.Parse);
+
+Write("Введите искомое значение: ");
+int.TryParse(ReadLine()!, out int searchnum);
+
+int[,] array = GetDDArray(ints[0], ints[1], ints[2], ints[3]);
+
+PrintDDArray(array);
+SearchNumber(array, searchnum);
+
+
+
+
+int[,] GetDDArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] Array = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Array[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+    return Array;
+}
+
+void PrintDDArray(int[,] inArrai)
+{
+    for (int i = 0; i < inArrai.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArrai.GetLength(1); j++)
+        {
+            Write($"{inArrai[i, j],5}");
+        }
+        WriteLine();
+    }
+
+}
+
+int SearchNumber(int[,] inArray, int num)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (array[i, j] == num)
+                WriteLine($"Элемент с координатами [{i},{j}] равный {inArray[i,j]} совпадает с искомым");
+            else
+                WriteLine($"Элемент [{i},{j}] не совпадает с искомым");
+        }
+    }
+    return num;
+}
--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -4,3 +4,60 @@
 //5 9 2 3
 //8 4 2 4
 //Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+Write("Введите параметры массива и искомое значение через пробел: ");
+
+int[] ints = Array.ConvertAll(ReadLine()!.Split(new char[] { ' ', '.', ',', ';', ':' }, StringSplitOptions.RemoveEmptyEntries), int.Parse);
+
+Write("Введите искомое значение: ");
+int.TryParse(ReadLine()!, out int searchnum);
+
+int[,] array = GetDDArray(ints[0], ints[1], ints[2], ints[3]);
+
+PrintDDArray(array);
+AverageNumber(array);
+
+int[,] GetDDArray(int rows, int columns, int minValue, int maxValue)
+{
+	int[,] Array = new int[rows, columns];
+	Random rnd = new Random();
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			Array[i, j] = rnd.Next(minValue, maxValue + 1);
+		}
+	}
+	return Array;
+}
+
+void PrintDDArray(int[,] inArrai)
+{
+	for (int i = 0; i < inArrai.GetLength(0); i++)
+	{
+		for (int j = 0; j < inArrai.GetLength(1); j++)
+		{
+			Write($"{inArrai[i, j],5}");
+		}
+		WriteLine();
+	}
+
+}
+
+int AverageNumber(int[,] inArray)
+{
+	double summ = 0;									// Сумма элементов по столбцам
+	
+	double[] array2 = new double[inArray.Length];		// Cоздаём одномерный массив для записи среднего арифметического по столбцам.
+
+	for (int i = 0; i < inArray.GetLength(1); i++)		//Проходим по столбцу
+	{
+		for (int j = 0; j < inArray.GetLength(0); j++)	// Проходим по строке
+		{
+			summ += inArray[i, j];						// Ссумаруем элементы в столбце
+			array2 = summ / inArray.GetLength(0);		// Вычисляем среднее арифметическое
+		}
+	}	
+	return array2;										// Возвращаем обномерный массив
+}
